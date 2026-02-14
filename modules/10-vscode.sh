2d1a6b1f40a28254127635b1b2c5ca876ef86134
test-user <psparks1@collin.edu>
2026-02-14 02:40:46 -0600

    fixed vscode install script

--- modules/10-vscode.sh
+++ modules/10-vscode.sh
@@ -10,12 +10,20 @@ fi
 
 log_info "Installing VS Code..."
 
-add_apt_repo_deb822 "vscode" \
-    "https://packages.microsoft.com/repos/code" \
-    "https://packages.microsoft.com/keys/microsoft.asc" \
-    "stable" \
-    "main"
+sudo apt-get install wget gpg &&
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg &&
+rm -f microsoft.gpg
 
-apt_install code
+sudo tee -a /etc/apt/sources.list.d/vscode.sources > /dev/null <<'EOF'
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+EOF
+
+apt update && apt_install code
 
 log_info "VS Code installed: $(code --version | head -1)"
